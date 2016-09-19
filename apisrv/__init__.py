9a74f308062ce40734a13e3e34fe2a611fb79c4b
Jonathan Yantis <yantisj@gmail.com>
2016-09-19 07:44:42 -0400

    Changed to MIT License

--- apisrv/__init__.py
+++ apisrv/__init__.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python
-# Copyright (c) 2016 "Jonathan Yantis"
+# Copyright (c) 2016 Jonathan Yantis
 #
-#    This program is free software: you can redistribute it and/or  modify
-#    it under the terms of the GNU Affero General Public License, version 3,
-#    as published by the Free Software Foundation.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    As a special exception, the copyright holders give permission to link the
-#    code of portions of this program with the OpenSSL library under certain
-#    conditions as described in each individual source file and distribute
-#    linked combinations including the program with the OpenSSL library. You
-#    must comply with the GNU Affero General Public License in all respects
-#    for all of the code used other than as permitted herein. If you modify
-#    file(s) with this exception, you may extend this exception to your
-#    version of the file(s), but you are not obligated to do so. If you do not
-#    wish to do so, delete this exception statement from your version. If you
-#    delete this exception statement from all source files in the program,
-#    then also delete it in the license file.
-#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 """
 Flask API Server
